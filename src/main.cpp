662889e4dfb28d07d06ada1c37fb637d1f49969c
Tommé Nöll <17678745+tnoell@users.noreply.github.com>
2020-06-17 01:27:45 +0200

    benchmarking loop

--- src/main.cpp
+++ src/main.cpp
@@ -44,54 +44,56 @@ int main(int argc, char *argv[])
     // Image image;
     // image.copyImage();
     int width = 3, height = 3;
-    if (argc == 3)
+    int runs = 1;
+    if (argc >= 3)
     {
         width = atoi(argv[1]);
         height = atoi(argv[2]);
     }
-    bool success = false;
-    uint64_t counter = 0;
-    while (!success)
+    if (argc == 4)
     {
-        auto start = high_resolution_clock::now();
-        
-        Tile tile("tiles/T.png", "T-Shape");
-        std::vector<Tile> tiles = {tile};
-        tile.addRotations(tiles);
-        
-        tiles.push_back(Tile("tiles/Curve.png", "Curve"));
-        tiles[tiles.size() - 1].addRotations(tiles);
-        
-        tiles.push_back(Tile("tiles/TBlue.png", "TBlue"));
-        tiles[tiles.size() - 1].addRotations(tiles);
-        
-        tiles.push_back(Tile("tiles/BlueStraight.png", "BlueStraight"));
-        tiles[tiles.size() - 1].addRotations(tiles, 1);
-        
-        tiles.push_back(Tile("tiles/BlueCross.png", "BlueCross"));
-        
-        tiles.push_back(Tile("tiles/Crossing.png", "Crossing"));
-        tiles[tiles.size() - 1].addRotations(tiles, 1);
-        
-        tiles.push_back(Tile("tiles/Clear.png", "Clear", 5));
-        
-        Tile::matchTiles(tiles);
-        Grid grid(tiles, width, height);
-        try
-        {
-            counter++;
-            success = true;
-            grid.run();
-        }
-        catch(std::string s)
-        {
-            std::cerr << s << '\n';
-            success = false;
-        }
-        
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<milliseconds>(stop - start);
-        std::cout  << "Took " << duration.count() << " milliseconds\n\n" << std::endl;
+        runs = atoi(argv[3]);
     }
-    std::cout << counter << " attempts\n";
+    std::chrono::milliseconds totalTime(0);
+    try
+    {
+        for (int i = 0; i < runs; i++)
+        {
+            auto start = high_resolution_clock::now();
+            
+            Tile tile("tiles/T.png", "T-Shape");
+            std::vector<Tile> tiles = {tile};
+            tile.addRotations(tiles);
+            
+            // tiles.push_back(Tile("tiles/Curve.png", "Curve"));
+            // tiles[tiles.size() - 1].addRotations(tiles);
+            
+            // tiles.push_back(Tile("tiles/TBlue.png", "TBlue"));
+            // tiles[tiles.size() - 1].addRotations(tiles);
+            
+            // tiles.push_back(Tile("tiles/BlueStraight.png", "BlueStraight"));
+            // tiles[tiles.size() - 1].addRotations(tiles, 1);
+            
+            // tiles.push_back(Tile("tiles/BlueCross.png", "BlueCross"));
+            
+            // tiles.push_back(Tile("tiles/Crossing.png", "Crossing"));
+            // tiles[tiles.size() - 1].addRotations(tiles, 1);
+            
+            // tiles.push_back(Tile("tiles/Clear.png", "Clear", 5));
+            
+            Tile::matchTiles(tiles);
+            Grid grid(tiles, width, height);
+            grid.run();
+            
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<milliseconds>(stop - start);
+            totalTime += duration;
+            std::cout  << "Took " << duration.count() << " milliseconds\n\n" << std::endl;
+        }
+        std::cout << "Total time: " << totalTime.count() << ", average: " << totalTime.count() / (float) runs << "\n";
+    }
+    catch(std::string s)
+    {
+        std::cerr << s << '\n';
+    }
 }
